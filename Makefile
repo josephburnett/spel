c1aa9925415e73d6395fdab45310a8ced02a9119
Joseph Burnett <josephburnett79@gmail.com>
2020-12-01 20:38:39 +0100

    Testing output for wasm.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-.goals = build
-.PHONY : build
+.goals = build serve
+.PHONY : build serve
 
 build :
 	cp $$GOROOT/misc/wasm/wasm_exec.js resources/
 	GOOS=js GOARCH=wasm go build -o resources/spel.wasm ./cmd/ui/main.go
+
+serve : build
+	cd resources ; python -m SimpleHTTPServer 8000
--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/josephburnett/spel/pkg/word"
+)
+
 func main() {
-	
+	doc := js.Global().Get("document")
+	app := doc.Call("getElementById", "app")
+	mutants, err := word.MutateTimes("testing", 10)
+	if err != nil {
+		panic(err)
+	}
+	app.Set("innerHTML", fmt.Sprintf("%v", mutants))
 }
