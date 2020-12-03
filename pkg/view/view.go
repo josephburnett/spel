40b3d746b63bae7e71c13d94d881b54ea13a5b32
Joseph Burnett <josephburnett79@gmail.com>
2020-12-03 19:59:45 +0100

    Delete unused view package.

--- pkg/view/view.go
+++
@@ -1,6 +0,0 @@
-package view
-
-// s := js.Global.Get("document").Call("createElement", "span")
-// f.Call("appendChild", s)
-// t := js.Global.Get("document").Call("createTextNode", "Hello World")
-// s.Call("appendChild", t)
