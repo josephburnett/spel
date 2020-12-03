155ccaeddb3c72a998f1ba42a8a662be76cc1362
Joseph Burnett <josephburnett79@gmail.com>
2020-12-03 21:14:29 +0100

    Load words from words.txt file.

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
 	"github.com/josephburnett/spel/pkg/app"
 )
 
 func main() {
-	s, err := app.NewSpel([]string{"testing"})
+	resp, err := http.Get("words.txt")
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	words := []string{}
+	for _, s := range strings.Split(string(body), "\n") {
+		if s == "" {
+			continue
+		}
+		words = append(words, s)
+	}
+	fmt.Printf("words: %v\n", words)
+	s, err := app.NewSpel(words)
 	if err != nil {
 		panic(err)
 	}
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -12,6 +12,7 @@ import (
 type Spel struct {
 	mux         sync.Mutex
 	words       []string
+	preview     bool
 	currentWord string
 	options     []string
 	score       int
@@ -22,8 +23,9 @@ type Spel struct {
 
 func NewSpel(words []string) (*Spel, error) {
 	s := &Spel{
-		words: words,
-		click: make(chan struct{}),
+		words:   words,
+		preview: true,
+		click:   make(chan struct{}),
 	}
 	s.nextWord()
 	return s, nil
@@ -33,6 +35,14 @@ func (s *Spel) clickFn(word string) func(js.Value, []js.Value) interface{} {
 	return func(_ js.Value, _ []js.Value) interface{} {
 		s.mux.Lock()
 		defer s.mux.Unlock()
+		defer func() {
+			s.click <- struct{}{}
+		}()
+		if s.preview {
+			s.preview = false
+			s.nextWord()
+			return nil
+		}
 		if word == s.currentWord {
 			s.score += 3
 			s.nextWord()
@@ -43,7 +53,6 @@ func (s *Spel) clickFn(word string) func(js.Value, []js.Value) interface{} {
 			}
 			s.catWidth = s.catWidth / 2
 		}
-		s.click <- struct{}{}
 		return nil
 	}
 }
@@ -63,6 +72,9 @@ func (s *Spel) nextWord() error {
 }
 
 func (s *Spel) Render() {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
 	doc := js.Global().Get("document")
 	app := doc.Call("getElementById", "app")
 	app.Set("style", "float:left;")
@@ -71,13 +83,23 @@ func (s *Spel) Render() {
 	top := doc.Call("createElement", "div")
 	top.Set("style", "float:left;clear:both;height:100px")
 	title := doc.Call("createElement", "h1")
-	title.Set("innerHTML", fmt.Sprintf("Score: %v", s.score))
+	if s.preview {
+		title.Set("innerHTML", fmt.Sprintf("These are your words"))
+	} else {
+		title.Set("innerHTML", fmt.Sprintf("Score: %v", s.score))
+	}
 	top.Call("appendChild", title)
 	app.Call("appendChild", top)
 
 	ul := doc.Call("createElement", "ul")
 	ul.Set("style", "width:300px;float:left;font-size:2em;clear:left")
-	for _, word := range s.options {
+	var words []string
+	if s.preview {
+		words = s.words
+	} else {
+		words = s.options
+	}
+	for _, word := range words {
 		li := doc.Call("createElement", "li")
 		li.Set("onclick", js.FuncOf(s.clickFn(word)))
 		text := doc.Call("createTextNode", word)
@@ -85,6 +107,10 @@ func (s *Spel) Render() {
 		ul.Call("appendChild", li)
 	}
 	app.Call("appendChild", ul)
+
+	if s.preview {
+		return // no cat
+	}
 
 	style := fmt.Sprintf("width:%vpx;float:left", s.catWidth)
 	if s.newCat {
